db6b1e60e1efea9b4b3eca290ecce4fbb9756bb7
Daniel Varcas <daniel.varcas@gmail.com>
2018-07-23 22:37:42 +0100

    added tests and user_id validation

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -8,4 +8,29 @@ class PostTest < ActiveSupport::TestCase
 	test "should be valid" do 
 		assert @post.valid?
 	end
+
+	test "title should be no longer than 300 characters" do
+		@post.title = "a" * 301
+		assert_not @post.valid?
+	end
+
+	test "link should be present" do
+		@post.link = "   "
+		assert_not @post.valid?
+	end
+
+	test "link format should be valid" do
+		@post.link = "hello lol"
+		assert_not @post.valid?
+	end
+
+	test "user ID should be present" do
+		@post.user_id = ""
+		assert_not @post.valid?
+	end
+
+	test "user ID should be an integer" do
+		@post.user_id = "one"
+		assert_not @post.valid?
+	end
 end
